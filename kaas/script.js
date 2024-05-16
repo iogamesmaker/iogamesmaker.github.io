32c38085e9e215edf6de516c036410d39434715b
iogamesmaker <51845955+iogamesmaker@users.noreply.github.com>
2024-05-16 10:46:17 +0200

    Update script.js

--- kaas/script.js
+++ kaas/script.js
@@ -9,31 +9,33 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 var cheeseCount = 0;
 
+var cheeseCount = 0;
+var cheeseLimit = 128;
+
 function duplicateCheese() {
-  if(cheeseCount >= 128) return;
-
-  var originalCheese = document.getElementById('kaasImg');
-  var cloneCheese = originalCheese.cloneNode(true);
-  cloneCheese.classList.add('duplicate');
-  
-  var maxX = window.innerWidth - originalCheese.width;
-  var maxY = window.innerHeight - originalCheese.height;
-  var randomX = Math.floor((Math.random() - 0.5) * maxX);
-  var randomY = Math.floor((Math.random() - 0.5) * maxY);
-  
-  cloneCheese.style.left = randomX + 'px';
-  cloneCheese.style.top = randomY + 'px';
-  
-  originalCheese.parentNode.appendChild(cloneCheese);
-  cheeseCount++;
-
-  if (cheeseCount >= 128) {
-    var cheeses = document.getElementsByClassName('duplicate');
+  if (cheeseCount >= cheeseLimit) {
+    // Remove the oldest cheese
+    var cheeses = document.querySelectorAll('.duplicate');
     cheeses[0].parentNode.removeChild(cheeses[0]);
     cheeseCount--;
   }
 
-  updateCheeseCountDisplay(); // Update cheese count display
+  var originalCheese = document.getElementById('kaasImg');
+  var cloneCheese = originalCheese.cloneNode(true);
+  cloneCheese.classList.add('duplicate');
+
+  // Generate random coordinates
+  var maxX = window.innerWidth - originalCheese.width;
+  var maxY = window.innerHeight - originalCheese.height;
+  var randomX = Math.floor((Math.random() - 0.5) * maxX);
+  var randomY = Math.floor((Math.random() - 0.5) * maxY);
+
+  // Set position
+  cloneCheese.style.left = randomX + 'px';
+  cloneCheese.style.top = randomY + 'px';
+
+  originalCheese.parentNode.appendChild(cloneCheese);
+  cheeseCount++;
 }
 
 function updateCheeseCountDisplay() {
