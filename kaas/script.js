02bc9a16907b0ac5d88e7a4f4c5763254b7c9e9a
iogamesmaker <51845955+iogamesmaker@users.noreply.github.com>
2024-06-05 13:09:09 +0200

    Update script.js

--- kaas/script.js
+++ kaas/script.js
@@ -1,39 +1,41 @@
 var cheeseCount = 1;
+var CPS = 0;
+var CPC = 1;
 var realCheeseCount = 1;
 var cheeseLimit = 256;
-var autoDuplicateEnabled = false;
 var autoDuplicateInterval;
 
 document.addEventListener('click', event => {
   duplicateCheese();
 });
 
-document.getElementById('increaseCheeseLimit').addEventListener('click', increaseCheeseLimit);
-document.getElementById('autoDuplicateCheese').addEventListener('click', enableAutoDuplicate);
+document.getElementById('autoclick').addEventListener('click', duplicate);
+document.getElementById('biggerclick').addEventListener('click', biggerclick);
 
 function duplicateCheese() {
+for(var i = 0; i < Math.min(CPC, cheeseLimit); i++) {
   if (cheeseCount >= cheeseLimit) {
-    var cheeses = document.querySelectorAll('.duplicate');
-    cheeses[0].parentNode.removeChild(cheeses[0]);
-    cheeseCount--;
+      var cheeses = document.querySelectorAll('.duplicate');
+      cheeses[0].parentNode.removeChild(cheeses[0]);
+      cheeseCount--;
+    }
+
+    var originalCheese = document.getElementById('kaasImg');
+    var cloneCheese = originalCheese.cloneNode(true);
+    cloneCheese.classList.add('duplicate');
+
+    var maxX = (window.innerWidth - originalCheese.width);
+    var maxY = window.innerHeight - originalCheese.height;
+    var randomX = Math.floor((Math.random() - 0.5) * maxX);
+    var randomY = Math.floor((Math.random() - 0.5) * maxY);
+    // aaaaaaaaaaaaa
+    cloneCheese.style.left = randomX + 'px';
+    cloneCheese.style.top = randomY + 'px';
+
+    originalCheese.parentNode.appendChild(cloneCheese);
+    cheeseCount += 1;
   }
-
-  var originalCheese = document.getElementById('kaasImg');
-  var cloneCheese = originalCheese.cloneNode(true);
-  cloneCheese.classList.add('duplicate');
-
-  var maxX = 0.6 * (window.innerWidth - originalCheese.width);
-  var maxY = window.innerHeight - originalCheese.height;
-  var randomX = Math.floor((Math.random() - 1.0) * maxX);
-  var randomY = Math.floor((Math.random() - 0.5) * maxY);
-
-  cloneCheese.style.left = randomX + 'px';
-  cloneCheese.style.top = randomY + 'px';
-
-  originalCheese.parentNode.appendChild(cloneCheese);
-  cheeseCount++;
-  realCheeseCount++;
-
+  realCheeseCount += CPC;
   updateCheeseCountDisplay();
 }
 
@@ -42,11 +44,18 @@ function updateCheeseCountDisplay() {
   cheeseCountDisplay.textContent = 'kaas!11!!: ' + realCheeseCount;
 }
 
-function enableAutoDuplicate() {
-  if (realCheeseCount >= 200 && !autoDuplicateEnabled) {
-    realCheeseCount -= 200;
-    autoDuplicateEnabled = true;
+function duplicate() {
+  if (realCheeseCount >= 3) {
+    CPS += 1;
+    realCheeseCount -= 3;
     updateCheeseCountDisplay();
-    autoDuplicateInterval = setInterval(duplicateCheese, 1000); // Duplicate cheese every second
+    autoDuplicateInterval = setInterval(duplicateCheese, 1.0 / CPS);
+  }
+}
+function biggerclick() {
+  if (realCheeseCount >= 20) {
+    CPC += 1;
+    realCheeseCount -= 20;
+    updateCheeseCountDisplay();
   }
 }
